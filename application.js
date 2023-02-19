ae6699b9a2291eaa309f66c9af9361b2bf0fa8c5
PabsFerdinand89 <39073417+PabsFerdinand89@users.noreply.github.com>
2023-02-18 23:40:41 -0600

    Add files via upload
    2nd attempt at shopping cart

--- application.js
+++ application.js
@@ -1,18 +1,20 @@
 var AddUpSubtotal = function (ele) {
-    var quantity = parseFloat($(ele).find('.quantity input').val());
-    var price = parseFloat(Math.round($(ele).children('.price').text() * 100) / 100).toFixed(2);
+    var quantity = Number(`${$(ele).find(".quantity input").val()}`);
+    var price = parseFloat(Math.round($(ele).children(".price").text() * 100) / 100).toFixed(2);
+
     var subtotal = quantity * price;
-    if (subtotal >= 0) {
-        $(ele).children('.subtotal').html('$${parseFloat(Math.round(subtotal * 100) / 100).toFixed(2)}');
-    } return subtotal;
-};
+        if (subtotal >= 0) {
+            $(ele).children('.subtotal').html(`$${parseFloat(Math.round(subtotal * 100) / 100).toFixed(2)}`);
+        } 
+        
+        return subtotal;
+    };
 
 var sum = function (acc, x) {
     return acc + x;
 };
 
-
-var updateShoppingCart = function(ele) {
+var updateShoppingCart = function() {
     var allSubtotals = [];
 
     $('tbody tr').each(function (i, ele) {
@@ -21,10 +23,11 @@ var updateShoppingCart = function(ele) {
     });
 
     if (allSubtotals.length == 0) {
-        $('#finalTotal').html.('$--.--');
+        $('#finalTotal').html(`$--.--`);
     } else {
         var cartTotal = allSubtotals.reduce(sum);
-        $('#finalTotal').html('$${parseFloat(Math.round(cartTotal * 100) / 100).toFixed(2)}');
+        $('#finalTotal').html(`$${parseFloat(Math.round(cartTotal * 100) / 100).toFixed(2)}`
+        );
       };
     };
 
@@ -35,7 +38,7 @@ var updateShoppingCart = function(ele) {
         $('body').on('click', '.remove', function (event) {
             $(this).closest('tr').remove();
             updateShoppingCart();
-        })
+        });
 
         var timeout;
             $('body').on('input', 'tr input', function () {
@@ -45,13 +48,18 @@ var updateShoppingCart = function(ele) {
                 }, 500);
             });
 
-        $('#addItem').on('submit', function (event) {
+        $('#addItem').on('add', function (event) {
             event.preventDefault();
             var item = $(this).children('.item name').val();
             var price = $(this).children('.price').val();
 
-            $('tbody').append('<tr><td class="item name"> ${item} </td> <td class="price">$${price}</td> <td class = "quantity"> <input type="number" value="1"/></td> <td><button class="btn btn-light btn-sm remove">remove</button></td>'
-            ,'<td class="subtotal"></td> </tr>');           
+            $('tbody').append('<tr>' + 
+                '<td class="item name">'${item}'</td>' +
+                '<td class="price">'$${price}'</td>' + 
+                '<td class="quantity"><input type="number" value="1"/></td>' + 
+                '<td><button class="btn btn-light btn-sm remove">remove</button></td>' +
+                '<td class="subtotal"></td>' + 
+                '</tr>');           
             
             updateShoppingCart();
             $(this).children('.item name').val('');
